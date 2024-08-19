bea3943b80fe2093d9c2f7170190d323e0b34557
SieGe0701 <116585928+SieGe0701@users.noreply.github.com>
2024-08-19 17:25:30 +0530

    Creating componenets

--- profile-website/src/App.js
+++ profile-website/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Services from './components/Services/Services';
+import Resume from './components/Resume/Resume'; 
+import Portfolio from './components/Portfolio/portfolio';
+import Contact from './components/contact/Contact';
+import Blog from './components/Blog/blog';
 
 const App = () => {
   return(
-    <div>App</div>
+    <>
+      <Sidebar />
+      <main className='main'>
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Blog />
+      </main>
+    </>
   )
 }
 
